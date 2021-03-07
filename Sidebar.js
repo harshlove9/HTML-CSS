50b9507e7dd5bbb0dc897f96ffc6cfb6ed4696d0
Harsh Shah <48030282+harshlove9@users.noreply.github.com>
2021-03-07 05:51:02 +0530

    Add files via upload

---
+++ Sidebar.js
@@ -0,0 +1,37 @@
+import MessageIcon from "@material-ui/icons/Message";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PeopleIcon from "@material-ui/icons/People";
+import AppsIcon from "@material-ui/icons/Apps";
+
+export const sidebarItemsData = [
+  {
+    icon: <MessageIcon />,
+    text: "Thred",
+  },
+
+  {
+    icon: <InboxIcon />,
+    text: "All DM",
+  },
+
+  {
+    icon: <DraftsIcon />,
+    text: "Mentions & Reactions",
+  },
+
+  {
+    icon: <BookmarkBorderIcon />,
+    text: "Save Items",
+  },
+
+  {
+    icon: <PeopleIcon />,
+    text: "Peoples and Groups",
+  },
+  {
+    icon: <AppsIcon />,
+    text: "More",
+  },
+];
